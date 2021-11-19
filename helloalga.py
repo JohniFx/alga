9a538f19d7bb90d5e6ff0a2476aa717388cfd9da
Johnifx <janos_torok@yahoo.com>
2021-11-19 19:37:45 +0100

    daily update

--- helloalga.py
+++ helloalga.py
@@ -7,18 +7,6 @@ print(datetime.now())
 # ignored config added
 config = configparser.Configparser()
 
-# more code here
-print(datetime.now())
-
-# modified locally
-print('this was modified locally')
-
-# modified in a new branch
-print('new bracnh modification')
-
-print('modification in main')
-
-print('daily modification')
 
 
 # Fri 19 Nov 2021 07:34:10 PM CET
