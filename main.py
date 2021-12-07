89a35b50253e4bed74bb0c7d76e0a33b8d74fc01
JohniFx <70256142+JohniFx@users.noreply.github.com>
2021-12-07 18:17:26 +0100

    cfg update

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 import cfg
 import trader
 import quant
+
 from datetime import datetime
 
 class Main():
     def __init__(self) -> None:
-        # update from lnx
-
+        
         # background activitites
         cfg.price_observers.append(self)
         cfg.transaction_observers.append(self)
         print(cfg.account.balance)
-
+        
         # update_kpi()
+        # update_tradeable_instruments()
         # check_instruments
+        
         # manage_positions
         # show_account
-
+        
     def on_tick(self, cp):
         msg = f"{datetime.now().strftime('%H:%M:%S')}"
         msg+= f" {cp['i']}: {cp['bid']:.5f} / {cp['ask']:.5f}"
         print(msg)
-
+        
     def on_data(self, data):
         print(data)
-
+        
     def schedule_tasks(self):
         pass
-
-
+    
+    def poll_account_update(self):
+        pass
+        
     def show_prices(self):
         r = cfg.ctx.pricing.get(cfg.ACCOUNT_ID, instruments='EUR_USD')
         prices = r.get('prices')
